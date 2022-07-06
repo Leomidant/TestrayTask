0bc7d0c0c1b5c335c7d0f7b442c750ba39e32ef2
beka.razmadze <beka.razmadze@testdevlab.com>
2022-07-06 14:35:02 +0400

    change py script

--- connection.js
+++ connection.js
@@ -1,11 +1,13 @@
-var teamcity = require("teamcity-rest-api");
+// var teamcity = require("teamcity-rest-api");
 
-var client = teamcity.create({
-    url: "http://localhost:8111",
-    username: "bekarazmadze",
-    password: "Bequnio2001!"
-});
+// var client = teamcity.create({
+//     url: "http://localhost:8111",
+//     username: "bekarazmadze",
+//     password: "Bequnio2001!"
+// });
 
-var test = client.projects.setParameter("AndroidToAndroid", "NETWORK_NAME","Unlimited")
-console.log(test)
+// var test = client.projects.setParameter("AndroidToAndroid", "NETWORK_NAME","Unlimited")
+// console.log(test)
+
+print("BEKA")
 
--- connection.py
+++ connection.py
@@ -1,5 +1,7 @@
-from pyteamcity import TeamCity
+# from pyteamcity import TeamCity
 
-tc = TeamCity("bekarazmadze", "Bequnio2001!", "http://localhost", 8111)
+# tc = TeamCity("bekarazmadze", "Bequnio2001!", "http://localhost", 8111)
 
-tc.get_project_by_project_id("AndroidToAndroid")
+# tc.get_project_by_project_id("AndroidToAndroid")
+
+print("BEKA")
