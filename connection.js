ed7c91f1383f591d6838fa6ff9fcbd237dc1500f
beka.razmadze <beka.razmadze@testdevlab.com>
2022-07-06 14:49:44 +0400

    change destination

--- connection.js
+++ connection.js
@@ -1,13 +1,12 @@
-// var teamcity = require("teamcity-rest-api");
+var teamcity = require("teamcity-rest-api");
 
-// var client = teamcity.create({
-//     url: "http://localhost:8111",
-//     username: "bekarazmadze",
-//     password: "Bequnio2001!"
-// });
+var client = teamcity.create({
+    url: "http://localhost:8111",
+    username: "bekarazmadze",
+    password: "Bequnio2001!"
+});
 
-// var test = client.projects.setParameter("AndroidToAndroid", "NETWORK_NAME","Unlimited")
-// console.log(test)
+var test = client.projects.setParameter("TestrayTask", "NETWORK_NAME","Unlimited")
+console.log(test)
 
-print("BEKA")
 
